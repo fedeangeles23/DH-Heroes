dc20214dab85945a93b4931495ec3088d71aa13c
FedeAngeles23 <fedeangeles23@gmail.com>
2022-06-20 23:11:48 -0300

    kklllñ

---
+++ README.md
@@ -0,0 +1,3 @@
+# heroes
+
+No lo pude subir a tiempo porque mi internet es una bosta y el vs code no me tomaba los commit y que se yo. Le mandé commits hasta que me agarro "el coso de los heroes"
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/babbage', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/babbage.html'));
+});
+
+app.get('/berners-lee', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/berners-lee.html'));
+});
+
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/clarke.html'));
+});
+
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/hamilton.html'));
+});
+
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/hopper.html'));
+});
+
+app.get('/lovelace', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/lovelace.html'));
+});
+
+app.get('/turing', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/turing.html'));
+});
+
+app.listen(3030, () => {
+console.log("Server listening on port 3030")
+});
